afb07755795806398690f0342e27638739a6e75d
ebront08 <ebront08@gmail.com>
2021-09-29 12:00:38 -0400

    added departments to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE employeemanagementsystem_db;
+-- seeds for Departments
+INSERT INTO departments (id, name)
+VALUES (1, "Sales"),
+       (2, "Software"),
+       (3, "Engineering"),
+       (4, "Finance"),
+       (5, "Legal");
