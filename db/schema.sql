9ad47ee246faf168418b458dbcfa2dd33c9226f5
ebront08 <ebront08@gmail.com>
2021-09-29 11:53:17 -0400

    added to employee table

--- db/schema.sql
+++ db/schema.sql
@@ -21,3 +21,8 @@ FOREIGN KEY (Departments_id) REFERENCES Departments (id)
 -- Employee table
 CREATE TABLE Employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  manager_id INT,
+  role_id INT,
+  
