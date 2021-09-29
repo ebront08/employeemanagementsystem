a190f0ea7ac96611d84fb551c812e18c2207c8ad
ebront08 <ebront08@gmail.com>
2021-09-29 12:40:48 -0400

    added function with array questions

--- index.js
+++ index.js
@@ -17,4 +17,42 @@ sqldb.connect(function(err) {
     console.log("Connected to MySQL")
     startPrompt();
 });
+// startPrompt function that starts question prompt + switch statements
+const startPrompt = () => {
+    return inquirer.prompt([{
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
+    }]).then(function(val) {
+        switch (val.action) {
+            case "View All Departments":
+                console.log(val.action);
+                viewAllDepartments();
+                break;
 
+            case "View All Roles":
+                viewAllRoles();
+                break;
+
+            case "View All Employees":
+                viewAllEmployees();
+                break;
+
+            case "Add Department":
+                addDepartment();
+                break;
+
+            case "Add Role":
+                addRole();
+                break;
+
+            case "Add Employee":
+                addEmployee();
+                break;
+
+            case "Update Employee Role":
+                updateEmployee();
+        }
+    });
+}
