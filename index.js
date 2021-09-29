f1827a91aa21a3564ac16d12267cba586e1a4ea4
ebront08 <ebront08@gmail.com>
2021-09-29 12:48:38 -0400

    edits to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 require('dotenv').config();
+const figlet = require('figlet');
 
 // Connection to SQL database
 const sqldb = mysql.createConnection({
@@ -56,3 +57,89 @@ const startPrompt = () => {
         }
     });
 }
+
+// viewAllDepartments function
+const viewAllDepartments = () => {
+    figlet("ALL  DEPARTMENTS", function(err, res) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(res)
+    })
+    const query = `SELECT * FROM departments`;
+    db.query(query,
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
+};
+
+// viewAllRoles function
+const viewAllRoles = () => {
+    figlet("ALL  ROLES", function(err, res) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(res)
+    })
+    const query = `SELECT roles.id, roles.title, roles.salary FROM roles`;
+    db.query(query,
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
+};
+
+// viewAllEmployees function
+const viewAllEmployees = () => {
+    figlet("ALL  EMPLOYEES", function(err, res) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(res)
+    })
+    const query = `SELECT employees.id, employees.first_name, employees.last_name, departments.name, roles.title, roles.salary, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employees INNER JOIN roles on roles.id = employees.role_id INNER JOIN departments on departments.id = roles.department_id left join employees e on employees.manager_id = e.id;`;
+    db.query(query,
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
+};
+
+// addDepartment function
+const addDepartment = () => {
+    figlet("ADD  DEPARTMENT", function(err, res) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(res)
+    })
+    inquirer.prompt([{
+        name: "Name",
+        type: "input",
+        message: "What department would you like to add?"
+    }]).then(function(res) {
+        const query = "INSERT INTO departments SET ?";
+        db.query(
+            query, {
+                name: res.Name
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                startPrompt();
+            }
+        )
+    })
+};
